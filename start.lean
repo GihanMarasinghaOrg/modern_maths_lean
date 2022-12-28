534a41f53a0a4caaa697ee3573b705c2a81e465f
Gihan Marasingha <gihan.marasingha@gmail.com>
2022-12-28 17:51:19 +0000

    Update Wed Dec 28 17:51:19 UTC 2022

--- start.lean
+++ start.lean
@@ -1,5 +1,9 @@
 /-
 
-Read the documentation in the right-most window.
+By opening this Lean file, you should have caused VSCode to open a
+Lean Infoview pane to the right.
+
+To continue, read the documentation in the right-most window by clicking
+the links for each chapter or using the 'Next' and 'Previous' navigation buttons.
 
 -/
