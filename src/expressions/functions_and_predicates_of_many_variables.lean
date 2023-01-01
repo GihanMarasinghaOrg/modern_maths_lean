0d3de980cb17ff10b71390cee7e64330ef75a22c
Gihan Marasingha <gihan.marasingha@gmail.com>
2023-01-01 23:33:36 +0000

    Update Sun Jan  1 23:33:36 UTC 2023

---
+++ src/expressions/functions_and_predicates_of_many_variables.lean
@@ -0,0 +1,22 @@
+/-
+# Chapter 2: Expressions
+## 2.7 Functions and predicates of many variables
+-/
+import data.int.basic tactic.norm_num
+
+def f(x : ℤ) (y : ℕ) := 2 * x + y
+
+
+/- PROBLEM:
+
+Use the `#check` command to find the type of `f`.
+
+-/
+
+
+/- PROBLEM:
+
+  Use the `#check` command to verify that `f 10` has type `ℕ → ℤ`.
+
+-/
+
---
+++ src/expressions/solutions/solutions_functions_and_predicates_of_many_variables.lean
@@ -0,0 +1,5 @@
+/-
+# Chapter 2: Expressions
+## 2.7 Functions and predicates of many variables
+-/
+import data.int.basic tactic.norm_num
